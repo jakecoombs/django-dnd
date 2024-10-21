7c1f75c73cde6e07f9ec15d26780b0cb17189dc7
jakecoombs <jakecoombs235@gmail.com>
2024-10-21 22:23:15 +0100

    Add description to class

--- character/admin.py
+++ character/admin.py
@@ -3,6 +3,31 @@ from django.contrib import admin
 from .models import Character, CharacterClass, Race
 
 
+class CharacterAdmin(admin.ModelAdmin):
+    fieldsets = (
+        (None, {"fields": ["name", "race", "character_class"]}),
+        ("Stats", {"fields": ["level", "armor_class", "hit_points"]}),
+        (
+            "Abilities",
+            {
+                "fields": [
+                    "strength",
+                    "dexterity",
+                    "constitution",
+                    "intelligence",
+                    "wisdom",
+                    "charisma",
+                ]
+            },
+        ),
+    )
+    search_fields = ("name",)
+
+
+class CharacterClassAdmin(admin.ModelAdmin):
+    search_fields = ("name",)
+
+
 class RaceAdmin(admin.ModelAdmin):
     fieldsets = (
         (None, {"fields": ["name", "parent_race", "description"]}),
@@ -11,6 +36,6 @@ class RaceAdmin(admin.ModelAdmin):
     search_fields = ("name",)
 
 
-admin.site.register(Character)
-admin.site.register(CharacterClass)
+admin.site.register(Character, CharacterAdmin)
+admin.site.register(CharacterClass, CharacterClassAdmin)
 admin.site.register(Race, RaceAdmin)
--- character/models.py
+++ character/models.py
@@ -25,6 +25,7 @@ class Race(models.Model):
 
 class CharacterClass(models.Model):
     name = models.CharField(max_length=50)
+    description = models.TextField()
 
     def __str__(self) -> str:
         return self.name
