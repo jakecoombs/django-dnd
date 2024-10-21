b48324c274b729f0e50507bd47f0f4d3f2ff8a98
jakecoombs <jakecoombs235@gmail.com>
2024-10-21 21:46:33 +0100

    Add fields to character

--- character/admin.py
+++ character/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
 
-from .models import Character
+from .models import Character, CharacterClass, Race
 
 admin.site.register(Character)
+admin.site.register(CharacterClass)
+admin.site.register(Race)
--- character/models.py
+++ character/models.py
@@ -15,17 +15,19 @@ class CharacterClass(models.Model):
         return self.name
 
 
-class Ability(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self) -> str:
-        return self.name
-
-
 class Character(models.Model):
     name = models.CharField(max_length=50)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     character_class = models.ForeignKey(CharacterClass, on_delete=models.CASCADE)
+    level = models.IntegerField(default=1)
+    armor_class = models.IntegerField()
+    hit_points = models.IntegerField()
+    strength = models.IntegerField()
+    dexterity = models.IntegerField()
+    constitution = models.IntegerField()
+    intelligence = models.IntegerField()
+    wisdom = models.IntegerField()
+    charisma = models.IntegerField()
 
     def __str__(self) -> str:
         return self.name
