618dad547cacfa357e83e7a45d7a7f5d6b12327d
jakecoombs <jakecoombs235@gmail.com>
2024-10-21 17:49:03 +0100

    Add extra models@ :

--- character/models.py
+++ character/models.py
@@ -1,8 +1,31 @@
 from django.db import models
 
 
+class Race(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class CharacterClass(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Ability(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self) -> str:
+        return self.name
+
+
 class Character(models.Model):
     name = models.CharField(max_length=50)
+    race = models.ForeignKey(Race, on_delete=models.CASCADE)
+    character_class = models.ForeignKey(CharacterClass, on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.name
