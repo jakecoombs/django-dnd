05326b5e16d351c72a9b18bed8457335b076287c
jakecoombs <jakecoombs235@gmail.com>
2024-10-21 22:49:57 +0100

    Add detail and list pages

--- character/views.py
+++ character/views.py
@@ -1,10 +1,15 @@
+from django.shortcuts import render
 from django.views import generic
 
-from character.models import Character
+from character.models import Character, CharacterClass, Race
 
 
-class IndexView(generic.ListView):
-    template_name = "character/index.html"
+def index(request):
+    return render(request, "character/index.html")
+
+
+class CharcterListView(generic.ListView):
+    template_name = "character/character_list.html"
 
     def get_queryset(self):
         """
@@ -13,6 +18,36 @@ class IndexView(generic.ListView):
         return Character.objects.all()
 
 
-class DetailView(generic.DetailView):
-    model = Character
-    template_name = "character/detail.html"
+class CharacterDetailView(generic.DetailView):
+    model = Race
+    template_name = "character/character.html"
+
+
+class CharacterClassDetailView(generic.DetailView):
+    model = CharacterClass
+    template_name = "character/characterclass.html"
+
+
+class CharacterClassListView(generic.ListView):
+    template_name = "character/characterclass_list.html"
+
+    def get_queryset(self):
+        """
+        Return all character classes.
+        """
+        return CharacterClass.objects.all()
+
+
+class RaceListView(generic.ListView):
+    template_name = "character/race_list.html"
+
+    def get_queryset(self):
+        """
+        Return all races.
+        """
+        return Race.objects.all()
+
+
+class RaceDetailView(generic.DetailView):
+    model = Race
+    template_name = "character/race.html"
