9dfb9feed66f254371403dc7f499d0acb167c451
jakecoombs <jakecoombs235@gmail.com>
2024-10-27 11:56:32 +0000

    Min values

--- character/models.py
+++ character/models.py
@@ -1,3 +1,4 @@
+from django.core.validators import MinValueValidator
 from django.db import models
 from django.utils.translation import gettext_lazy as _
 
@@ -35,15 +36,15 @@ class Character(models.Model):
     name = models.CharField(max_length=50)
     race = models.ForeignKey(Race, on_delete=models.CASCADE)
     character_class = models.ForeignKey(CharacterClass, on_delete=models.CASCADE)
-    level = models.IntegerField(default=1)
-    armor_class = models.IntegerField()
-    hit_points = models.IntegerField()
-    strength = models.IntegerField()
-    dexterity = models.IntegerField()
-    constitution = models.IntegerField()
-    intelligence = models.IntegerField()
-    wisdom = models.IntegerField()
-    charisma = models.IntegerField()
+    level = models.IntegerField(default=1, validators=(MinValueValidator(1),))
+    armor_class = models.IntegerField(validators=(MinValueValidator(0),))
+    hit_points = models.IntegerField(validators=(MinValueValidator(0),))
+    strength = models.IntegerField(validators=(MinValueValidator(0),))
+    dexterity = models.IntegerField(validators=(MinValueValidator(0),))
+    constitution = models.IntegerField(validators=(MinValueValidator(0),))
+    intelligence = models.IntegerField(validators=(MinValueValidator(0),))
+    wisdom = models.IntegerField(validators=(MinValueValidator(0),))
+    charisma = models.IntegerField(validators=(MinValueValidator(0),))
 
     def __str__(self) -> str:
         return self.name
